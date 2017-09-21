617f444b4361a37e27d0ead76cb262756ae57097
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-09-21 16:17:19 +0100

    Add tests for sarsop and qmpd solvers

--- hri_simulator.jl
+++ hri_simulator.jl
@@ -148,16 +148,16 @@ function basic_test(;re_calc_interval=0, num_iter=1000, out_file=-1, reward_chan
     return reward_history
 end
 
-f1 = open("sarsop.yaml", "a")
-for i = 1:100
+f1 = open("sarsop_random_1.yaml", "a")
+for i = 1:1000
     print(".")
-    basic_test(re_calc_interval=1, num_iter=100, out_file=f1, solver_name="sarsop")
+    basic_test(re_calc_interval=1, num_iter=1000, out_file=f1, solver_name="sarsop")
 end
 close(f1)
-f1 = open("qmdp.yaml", "a")
-for i = 1:100
+f1 = open("qmdp_random_1.yaml", "a")
+for i = 1:1000
     print(".")
-    basic_test(re_calc_interval=1, num_iter=100, out_file=f1, solver_name="qmdp")
+    basic_test(re_calc_interval=1, num_iter=1000, out_file=f1, solver_name="qmdp")
 end
 close(f1)
 
