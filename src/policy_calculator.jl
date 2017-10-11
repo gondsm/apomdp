c3fb1a7a7a5fe1b4ce6e8e4a1f6ca361a17bd6a1
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-10-11 11:34:39 +0100

    First working version of the service callback, missing data insertion into the POMDP.

--- src/policy_calculator.jl
+++ src/policy_calculator.jl
@@ -1,10 +1,10 @@
 #!/usr/bin/env julia06
+
 # The main goal of this script is to serve as an interface for a ROS system using aPOMDP as its decision-making
 # technique.
 # It should interface with ROS by receiving the observations from the underlying system and relaying them to 
 # aPOMDP, control the re-calculation of the policy, and by packing and sending the policy in an appropriate
 # message.
-
 
 # Imports, includes et al
 include("./apomdp.jl")
@@ -16,6 +16,7 @@ using RobotOS
 rostypegen()
 using apomdp.srv
 
+
 # Global vars: given that we're using a ROS service, there's some need
 # for global state. This could be solved with a singleton in an OOP,  but
 # alas, this is not the case.
@@ -26,19 +27,31 @@ pomdp = aPOMDP("isvr")  # The aPOMDP object that we'll be using throughout execu
 policy = nothing        # The most up-to-date policy
 solve_flag = true       # A global flag that indicates whether we want to solve the POMDP
 
+
 # Callback for serving the service. It gets the action from the policy, given
 # the current observation, and returns it.
 function srv_cb(req::GetActionRequest)
-    println("Service called!")
-    resp = GetActionResponse()
-    println("Got a state:")
+    # Get correct action for this observation
+    # Pack state into Julia array (could probably be better optimized)
+    state = []
     for s in req.observation
-        println(s)
+        append!(state, convert(Int64, s))
     end
-    resp.action = 1
+    # Get action
+    a = action(policy, apomdpDistribution(pomdp, state))
+
+    # Pack into response
+    resp = GetActionResponse()
+    resp.action = a
+    
+    # Add data to the POMDP
+    # TODO
     global solve_flag = true
+
+    # Return the response
     return resp
 end
+
 
 # Simple function for updating the system's policy
 function update_policy()
@@ -48,6 +61,8 @@ function update_policy()
     println("done!")
 end
 
+
+# And a main function
 function main()
     # Initialize ROS node
     println("Initializing aPOMDP")
