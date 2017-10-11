234315efeb4713126bdbb90ac6818da8a1afa0a1
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-10-11 11:07:58 +0100

    Add a first version of the ROS service for communicating observations and actions.

--- src/policy_calculator.jl
+++ src/policy_calculator.jl
@@ -1,49 +1,47 @@
+#!/usr/bin/env julia06
 # The main goal of this script is to serve as an interface for a ROS system using aPOMDP as its decision-making
 # technique.
 # It should interface with ROS by receiving the observations from the underlying system and relaying them to 
 # aPOMDP, control the re-calculation of the policy, and by packing and sending the policy in an appropriate
 # message.
-include("./apomdp.jl")
 
+
+# Imports, includes et al
+include("./apomdp.jl")
 using RobotOS
 
-# Initialize POMDP
-println("Initializing aPOMDP")
-pomdp = aPOMDP()
 
-# Initialize values and rewards
-integrate_transition(pomdp, [1,1], [1,2], 1)
-#integrate_transition(pomdp, [1,2], [1,2], 3)
-integrate_transition(pomdp, [1,1], [1,3], 2)
-#integrate_transition(pomdp, [1,3], [1,3], 2)
-set_state_value(pomdp, [1,2], 10)
-set_state_value(pomdp, [1,3], 20)
-calculate_reward_matrix(pomdp)
+# Create our ROS types
+@rosimport apomdp.srv: GetAction
+rostypegen()
+using apomdp.srv
 
-# Initialize solver
-println("Initializing solver")
-solver = QMDPSolver()
-#solver = SARSOPSolver() # Brings a whole new host of problems
 
-# Get a policy
-print("Solving... ")
-policy = solve(solver, pomdp)
-println("Done!")
-
-# Create a belief updater 
-println("Creating belief updater")
-belief_updater = updater(policy)
-
-# Run a simulation
-println("Simulating POMDP")
-history = simulate(HistoryRecorder(max_steps=20), pomdp, policy, belief_updater)
-
-# look at what happened
-for (s, b, a, o, r) in eachstep(history, "sbaor")
-    println("State was $s,")
-    println("belief was $b,")
-    println("action $a was taken,")
-    println("reward $r was received")
-    println("and observation $o was received.\n")
+# Callback for serving the service. It gets the action from the policy, given
+# the current observation, and returns it.
+function srv_cb(req::GetActionRequest)
+    println("Service called!")
+    resp = GetActionResponse()
+    println("Got a state:")
+    for s in req.observation
+        println(s)
+    end
+    resp.action = 1
+    return resp
 end
-println("Discounted reward was $(discounted_reward(history)).")
+
+function main()
+    # Initialize ROS node
+    init_node("policy_calculator")
+
+    # Create the service server object
+    const srv_action = Service("apomdp/get_action", GetAction, srv_cb)
+
+    # "spin" while waiting for requests
+    println("Going into spin!")
+    while ! is_shutdown()
+        rossleep(Duration(0.1))
+    end
+end
+
+main()
