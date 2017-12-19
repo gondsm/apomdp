02f500339daab5f66939ba65e4922931f32c2545
Gonçalo S. Martins <gmartins@isr.uc.pt>
2017-12-19 10:34:46 +0000

    Slight code cleanup

--- src/agent.jl
+++ src/agent.jl
@@ -20,7 +20,6 @@ include("./apomdp.jl")
 using RobotOS
 
 # Create our ROS types
-#TODO: do we need more services? topics to be created? 
 @rosimport apomdp.srv: Act 
 @rosimport apomdp.msg: shared_data
 rostypegen()
@@ -28,112 +27,67 @@ using apomdp.srv
 using apomdp.msg
 
 
-# Global vars: given that we're using a ROS service, there's some need
-# for global state. This could be solved with a singleton in an OOP,  but
-# alas, this is not the case.
-# In essence, we maintain the pomdp object, the current policy and a flag
-# to indicate it's solving time, allowing the service callback to add data 
-# to the pomdp object and not have to solve it immediatelly
-#TODO: we need to change in the aPOMDP the reward function as the isvr is not really the rewarding we are going to use 
-pomdp = aPOMDP("isvr", 1, [3, 3, 3], 5)  # The aPOMDP object that we'll be using throughout execution
-policy = nothing        # The most up-to-date policy
-solve_flag = true       # A global flag that indicates whether we want to solve the POMDP
-prev_state = nothing    # The previous state the user was in
-prev_action = nothing   # The previous action taken by the system
+# Global vars
 
 
-# Global vars for log keeping: These are only written to, and are used
-# to generate the logs necessary for plotting.
-#TODO: do we need more vars for the logs? 
-num_iter = 0
-v_s = Dict()
-reward_history = []
-state_history = []
-action_history = []
-entropy_history = []
+# Agent-specific actions
+#this function will return the v(s)
+function get_v_s(state)
+    #TODO: v(s) = -2n_victim * -n_depries * n_fire
+end 
 
-# Temporary function to attribute value to states
-function set_valuable_states(pomdp::aPOMDP, n_v_s=1)
-    # Generates random state values for simulation purposes and returns the value function used
-    v_s = Dict()
-    for state in pomdp.states
-        v = 10*state[1]
-        set_state_value(pomdp, state, v, 1)
-        v_s[state] = v
-    end
-    calculate_reward_matrix(pomdp)
-    return v_s
-end
+# this function will retun cost vector for all actions for this agent
+#
+function calc_cost_vector(belief, ability_vector) 
 
-# Callback for serving the service. It gets the action from the policy, given
-# the current observation, and returns it.
-# function srv_cb(req::GetActionRequest)
-#     # Pack state into Julia array (could probably be better optimized)
-#     state = [convert(Int64, s) for s in req.observation]
-
-#     # Check that the input conforms to our current structure
-#     if size(state)[1] != size(pomdp.state_structure)[1]
-#         println("Got a wrong-size state! State: $state")
-#         println("Current state structure is $(pomdp.state_structure)")
-#         return false
-#     else
-#         invalid = [state[i] > pomdp.state_structure[i] || state[i] < 1 for i in 1:size(state)[1]]
-#         for v in invalid
-#             if v == true
-#                 println("Got an out of bounds value! State: $state")
-#                 println("Current state structure is $(pomdp.state_structure)")
-#                 return false
-#             end
-#         end
-#     end
-
-#     # Get action
-#     a = action(policy, apomdpDistribution(pomdp, state))
-
-#     # Update logs
-#     global num_iter += 1
-#     append!(state_history, state)
-#     append!(action_history, a)
-#     append!(reward_history, reward(pomdp, state, a))
-#     append!(entropy_history, calc_average_entropy(pomdp))
-
-#     # Pack into response
-#     resp = GetActionResponse()
-#     resp.action = a
-
-#     # Integrate transition
-#     if prev_state != nothing && prev_action != nothing
-#         println("Integrating transition!")
-#         integrate_transition(pomdp, prev_state, state, prev_action)
-#     end
-
-#     # Update globals
-#     global prev_state = state
-#     global prev_action = a
-#     global solve_flag = true
-
-#     # Return the response
-#     return resp
-# end
+end 
+######
 
 
+# APOMDP-dependent functions (maths)
 # fuse belief function
 function fuse_beliefs(beliefs_vector)
 
 end
 
 # fuse transistions function 
-function fuse_transitions(transistions_vector)
+function fuse_transitions(transitions_vector)
 
 end
 
+# function used to solve pomdp and return policy 
+function get_policy(fused_T, c_vector)
+    #get the v_s
+    #v = get_v_s(state)
+    #TODO: this function will return the value of state v(s) 
+end
+
+# this function is called for decision_making and it will return an action 
+function get_action(policy,fused_b)
+    # Get action
+    #a = action(policy, apomdpDistribution(pomdp, state))
+end 
+
+# will update the belief and it will return beliefs_vector
+function update_belief(observation,action,belief, transistion)
+
+end 
+
+# this function will return the transitions_vector
+function learn(belief, action, previous_b) 
+    # integrate_transition(pomdp, prev_state, state, prev_action)
+end 
+######
+
+
+# ROS-related functions
 # call simulation (function ?? or just from the main)
 function act(action)
 
 end
 
 # publish to broadcast topic (function ?? or just from the main) 
-function share_data(beliefs_vector, transistions_vector)
+function share_data(beliefs_vector, transitions_vector)
 
 end
 
@@ -143,44 +97,13 @@ function share_data_cb(msg)
 
 end
 
-#this function will return the v(s)
-function get_v_s(state)
-    #TODO: v(s) = -2n_victim * -n_depries * n_fire
-end 
-
-# function used to solve pomdp and return policy 
-function get_policy(fused_T)
-    #get the v_s
-    #v = get_v_s(state)
-    #TODO: this function will return the value of state v(s) 
-end
-
-# this function is called for decision_making and it will return an action 
-function get_action(policy,fused_b)
-
-end 
-
-# will update the belief and it will return beliefs_vector
-function update_belief(observation,action,belief, transistion)
-
-end 
-
-# this function will return the transistions_vector
-function learn(belief, action, previous_b) 
-
-end 
-
-# this function will retun cost vector for all actions for this agent
-#
-function calc_cost_vector(belief, ability_vector) 
-
-end 
 
 # And a main function
 function main()
     #TODO: get the agent_id from somewhere
     agent_id = 0
     agent_index = agent_id + 1
+
     # Initialize ROS node
     println("Initializing bPOMDP")
     init_node("agent_$(agent_id)")
@@ -190,58 +113,53 @@ function main()
     service_client = ServiceProxy{Act}("act")
 
     # Subscribe to the agent's shared_data topic
-    #create publisher, and a subscriber 
+    # Create publisher, and a subscriber 
     pub = Publisher{shared_data}("broadcast", queue_size=10)
     sub = Subscriber{shared_data}("shared_data/$(agent_id)", share_data_cb, queue_size=10)
 
-    # Initialize valuable states
-    #TODO
-
-    #Initialize variables 
-    #TODO: do it proberly 
+    # Initialize variables 
+    # TODO: do it properly 
     fused_T = nothing
     beliefs_vector = [nothing, nothing, nothing, nothing] #TODO: should be global because it will be updated by the callback
-    transistions_vector = [nothing, nothing, nothing, nothing] #TODO: should be global because it will be updated by the callback
+    transitions_vector = [nothing, nothing, nothing, nothing] #TODO: should be global because it will be updated by the callback
     ability_vector = [nothing, nothing, nothing, nothing] #TODO: read this from configuration (agent.yaml) 
+
     # "spin" while waiting for requests
     println("Going into spin!")
     while ! is_shutdown()
-
-
-        # get the cost 
+        # Get the cost 
         c_vector = calc_cost_vector(beliefs_vector[agent_index], ability_vector)
 
-        # solve
+        # Solve
         policy = get_policy(fused_T, c_vector)
 
         # Call fuse belief function
         fused_b = fuse_beliefs(beliefs_vector)
         #TODO: make beliefs_vector global variable 
 
-        # decision_making
+        # Decision_making (action selection)
         action = get_action(policy,fused_b)
 
-        #observation 
+        # Act and receive an observation 
         observation = act(action)
 
-        #to save the prvious belief 
-        previous_b = beliefs_vector[agent_index]
-
-        # update belief - on the local 
-        beliefs_vector[agent_index]= update_belief(observation,action,beliefs_vector[agent_index], transistions_vector[agent_index])
+        # Update belief - on the local 
+        previous_b = beliefs_vector[agent_index] # save the previous belief 
+        beliefs_vector[agent_index]= update_belief(observation,action,beliefs_vector[agent_index], transitions_vector[agent_index])
         #TODO: rethink of we should use the local transition or the fused one
 
-        # update_belief
+        # Fuse beliefs
         fused_b = fuse_beliefs(beliefs_vector)
 
-        # learn - based on the local 
-        transistions_vector[agent_index] = learn(beliefs_vector[agent_index], action, previous_b)
-        #TODO: decide whether to use the fused_b or local belief 
+        # Learn - based on the local 
+        transitions_vector[agent_index] = learn(beliefs_vector[agent_index], action, previous_b)
+        # TODO: decide whether to use the fused_b or local belief 
 
-        fused_T = fuse_transitions(transistions_vector)
+        # Fuse new transition with everyone else's
+        fused_T = fuse_transitions(transitions_vector)
 
-        # publish something to "broadcast" topic
-        share_data(beliefs_vector, transistions_vector)
+        # Publish something to "broadcast" topic
+        share_data(beliefs_vector, transitions_vector)
         # TODO: decide when to do this  
         # TODO: find a way to avoid fusing repeated information - depends on the time and the type of info to fuse (local or fused)
         # TODO: useing of time stamp of when every agent updated 
@@ -252,25 +170,7 @@ function main()
 
     println("Agent exiting!")
 
-
-    
-
-
-    # Write logs to file
-    # out_file = open("sr_results.yaml", "a")
-    # log_execution(out_file,
-    #               num_iter,
-    #               reward_change_interval,
-    #               re_calc_interval,
-    #               exec_time,
-    #               v_s,
-    #               action_history,
-    #               entropy_history,
-    #               state_history,
-    #               reward_history,
-    #               scenario,
-    #               pomdp)
-    # close(out_file)
+    # TODO: Write logs
 end
 
 
