be18be429a5168cd32f0e335cb364cd9cafe0e5d
hend.altair <hend.altair@kustar.ac.ae>
2018-05-30 12:12:17 +0400

    update

--- src/test_call.jl
+++ src/test_call.jl
@@ -11,18 +11,13 @@ w_states1 = world_states[:]
 alpha_states = [a_states1; w_states1]
 
 state_structure=[2,3]
-reward_name = "svr"
+#reward_name = "svr"
 weight = normalize(rand(1),1)
-agents_size=2
-agents_specfi=2
-nodes_num=2
-world_specfi=3
+#agents_size=2
+#agents_specfi=2
+#nodes_num=2
+#world_specfi=3
 
-state_values_dict::Dict
-
-
-#pomdp = aPOMDP(reward_name, 1, state_structure, 5,weight,agents_size,agents_specfi,nodes_num,world_specfi)
-
-pomdp = aPOMDP(5,state_values_dict,1,normalize(rand(n_v_s)),transition_dict, reward_dict,0.95,states,state_indices,state_structure,"svr",agents_size,agents_specfi,nodes_num,world_specfi)
+pomdp = aPOMDP(5,state_values_dict,1,weight,transition_dict, reward_dict,0.95,states,state_indices,state_structure,"svr",2,2,2,3)
 
 #state_a_to_b(pomdp,alpha_states)
